123bed088ec90bcc85aaa5b4c90959d77ee36f09
SantiagoGalindoHernandez19 <124641609+SantiagoGalindoHernandez19@users.noreply.github.com>
2023-03-26 20:44:53 -0500

    Add files via upload

---
+++ punto 2, reto 7.py
@@ -0,0 +1,14 @@
+
+
+x = 1 #Asignamos valor 
+while x <=999: #condicion de bucle
+    if x % 2 == 1:
+        print(x)  #Muestre factores inpares
+    x += 1 #Variable x incrementa en 1
+
+
+x = 1 #Asignamos valor 
+while x <=1000: #condicion de bucle
+    if x % 2 == 0:
+        print(x)  #Muestre factores pares
+    x += 1 #Variable x incrementa en 1
