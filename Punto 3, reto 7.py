15b3c0464e571a8b64eafc81ec5937b54324e99c
SantiagoGalindoHernandez19 <124641609+SantiagoGalindoHernandez19@users.noreply.github.com>
2023-03-26 21:43:19 -0500

    Add files via upload

---
+++ Punto 3, reto 7.py
@@ -0,0 +1,5 @@
+x = int(input("Ingrese un número: ")) # Pedir ingreso de un numero x
+while (x>=2): 
+    if x%2 ==0: #condición del bucle para definir los numeros pares
+        print(x) #Variable impresion
+    x-=2   #Sea descendente   
